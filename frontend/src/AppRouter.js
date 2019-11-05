7e1216bdcc10aae2c8f81de9be8684c0c474b456
Kelen Y. Yafuso <kyyafuso@gmail.com>
2019-11-04 18:53:09 -0800

    Adds FAQ page to app

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './views/HomePage/'
 import SignUp from './views/SignUp/'
+import FAQ from './views/FAQ/'
 
 const AppRouter = () => {
 
   return (<>
     <Router >
       <Switch>
+        <Route path="/_/FAQ" component={FAQ} />
         <Route path="/_/signup" component={SignUp} />
         <Route path="/_/homepage" component={HomePage} />
         <Route path="/" component={HomePage} />
