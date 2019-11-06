63d7c5db6fdd7919077cad69524507109cf874fc
Kelen Y. Yafuso <kyyafuso@gmail.com>
2019-11-05 20:27:38 -0800

    loginstuff

---
+++ frontend/src/views/Login/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { Button, CssBaseline, Container, TextField, Typography } from '@material-ui/core'
+import { withRouter } from 'react-router-dom'
+import NavBar from '../../components/NavBar'
+import { withStyles } from '@material-ui/core/styles'
+import styles from '../../components/styles'
+
+const Login = ({classes, history}) => {
+  const [ values, setValues ] = useState({
+    email: '',
+    password: '',
+  })
+
+  const inputChange = e => {
+    const { name, value } = e.targe
+    setValues({...values, [name]: value})
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    const data = {...values}
+    data.password_confirmation = values.password
+    history.push('/')
+    }
+
+  return(
+    <>
+      <NavBar />
+      <CssBaseline />
+      <Container maxWidth='sm'>
+        <Typography>
+          Log-in
+        </Typography>
+        <form onSubmit={onSubmit} className={classes.form}>
+          <TextField
+            className={classes.textField}
+            name="email"
+            onChange={inputChange}
+            label="Email"
+          />
+          <TextField
+            className={classes.textField}
+            name="password"
+            type='password'
+            onChange={inputChange}
+            label="Password"
+          />
+          <Button variant='contained' type="submit">Submit</Button>
+        </form>
+      </Container>
+    </>
+  )
+}
+
+export default withStyles(styles)(withRouter(Login))
